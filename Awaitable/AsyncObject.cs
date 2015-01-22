c098b9453b56ead4f5a4c8a101684ee36d85c4d0
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-01-22 19:52:29 +0100

    Implemented Invoker
    -> for performance reasons

--- Awaitable/AsyncObject.cs
+++ Awaitable/AsyncObject.cs
@@ -19,8 +19,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Dynamic;
-using System.Linq;
-using System.Reflection;
+using Invocation;
 
 namespace Awaitable
 {
@@ -33,96 +32,14 @@ namespace Awaitable
             _value = value;
         }
 
-        // ReSharper disable once StaticFieldInGenericType : Generic Cache
-        private static PropertyDescriptorCollection _properties;
-
-        private static PropertyDescriptorCollection Properties
+        private Invoker GetInvoker(InvokeMemberBinder binder, IList<object> args)
         {
-            get
-            {
-                return _properties ?? (_properties = TypeDescriptor.GetProperties(typeof(T)));
-            }
-        }
+            var callers = TypeInfo<T>.Methods[binder.Name];
+            var tuple = CallerSelector.SelectMethod(binder, args, callers);
+            var method = tuple.Item1;
+            var actualArguments = tuple.Item2;
 
-
-        // ReSharper disable once StaticFieldInGenericType : Generic Cache
-        private static ILookup<string, MethodInfo> _methodInfo;
-        private static ILookup<string, MethodInfo> Info
-        {
-            get
-            {
-                return _methodInfo ??
-                       (_methodInfo = typeof(T).GetMethods(BindingFlags.Public | BindingFlags.Instance).ToLookup(x => x.Name));
-            }
-        }
-
-        private static bool CompareParameters(IReadOnlyList<Argument> parameters,
-            IEnumerable<ParameterInfo> methodParameters, out IEnumerable<Argument> arguments)
-        {
-            var selectableArguments = methodParameters.Select(x => new SelectableArgument(x))
-                                                      .ToList();
-
-            arguments = selectableArguments;
-
-            for (var i = 0; i < parameters.Count; i++)
-            {
-                var parameter = parameters[i];
-
-                var methodParam = parameter.HasName
-                    ? selectableArguments.FirstOrDefault(x => x.Name == parameter.Name)
-                    : selectableArguments.ElementAtOrDefault(i); //Unnamed params come first and in order, select by index.
-
-                //Check if a param has been found.
-                if (methodParam == null) return false;
-
-                if (parameter.Type == null)
-                {
-                    //If null, null has been passed so the type has to be a value type.
-                    if (!methodParam.Type.IsValueType) return false;
-                }
-                else
-                {
-                    //If not null, parameter has to be assignable to 
-                    if (!methodParam.Type.IsAssignableFrom(parameter.Type)) return false;
-                }
-
-                //Override value with passed value
-                methodParam.Value = parameter.Value;
-                methodParam.Selected = true;
-            }
-
-            return selectableArguments.All(x => x.Selected || x.HasDefaultValue);
-        }
-
-        private static Invoker GetInvoker(InvokeMemberBinder binder, IList<object> args)
-        {
-            var list = new List<Argument>();
-            var names = new Stack<string>(binder.CallInfo.ArgumentNames);
-
-            //Named parameters can always be mapped directly on the last parameters.
-            for (var i = args.Count - 1; i >= 0; i--)
-            {
-                var argument = args[i];
-                string name = null;
-
-                if (names.Count > 0)
-                    name = names.Pop();
-
-                var arg = new Argument(name, argument);
-                list.Insert(0, arg);
-            }
-
-            var methods = Info[binder.Name];
-            IEnumerable<Argument> actualArguments = null;
-
-            var method = (from methodInfo in methods
-                          where CompareParameters(list, methodInfo.GetParameters().ToList(), out actualArguments)
-                          select methodInfo).FirstOrDefault();
-
-            if (method == null)
-                return null;
-
-            return new Invoker(method, actualArguments.Select(x => x.Value).ToArray());
+            return new Invoker(method, actualArguments);
         }
 
 
@@ -148,32 +65,20 @@ namespace Awaitable
 
         public override bool TryGetMember(GetMemberBinder binder, out object result)
         {
-            var proprety = Properties[binder.Name];
-            if (proprety == null)
-            {
-                result = null;
-                return false;
-            }
-
-            result = proprety.GetValue(_value);
+            result = TypeInfo<T>.GetProperty(_value, binder.Name); ;
             return true;
         }
 
         public override bool TrySetMember(SetMemberBinder binder, object value)
         {
-            var proprety = Properties[binder.Name];
-            if (proprety == null)
-            {
-                return false;
-            }
-
-            proprety.SetValue(_value, value);
-            return true;
+            //TODO: Setters
+            //proprety.SetValue(_value, value);
+            return false;
         }
 
         public override bool TryConvert(ConvertBinder binder, out object result)
         {
-            var valueType = typeof(T);
+            var valueType = Constants.Typed<T>.OwnerType;
 
             if (binder.ReturnType == valueType)
             {
--- Awaitable/Invoker.cs
+++ Awaitable/Invoker.cs
@@ -16,27 +16,35 @@
 // 
 #endregion
 
-using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Invocation;
 
 namespace Awaitable
 {
     class Invoker
     {
-        private readonly object[] _arguments;
-        private readonly MethodInfo _method;
+        private readonly IEnumerable<object> _arguments;
+        private readonly MethodCaller _caller;
 
-        public Invoker(MethodInfo method, object[] arguments)
+        public Invoker(MethodCaller caller, IEnumerable<object> arguments)
         {
-            _method = method;
+            _caller = caller;
             _arguments = arguments;
         }
 
         public object Invoke(object value)
         {
-            return typeof(Task).IsAssignableFrom(_method.ReturnType)
-                ? _method.Invoke(value, _arguments) //Already Async, return as is.
-                : Task.Run(() => _method.Invoke(value, _arguments));
+            var arguments = _arguments.ToList();
+
+            if (!_caller.IsStatic)
+                arguments.Insert(0, value);
+
+            if (_caller.IsAsync)
+                return _caller.Call(arguments);
+
+            return Task.Run(() => _caller.Call(arguments));
         }
     }
 }
